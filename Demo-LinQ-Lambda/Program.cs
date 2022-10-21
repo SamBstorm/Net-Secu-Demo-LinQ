acfd573d2aa908ed0ef16ca54bebc8fee99d46ca
Samuel Legrain <samuel.legrain@bstorm.be>
2022-10-21 10:23:03 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-LinQ-Lambda/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Demo_LinQ_Lambda
+{
+    delegate int myDel(int x);              //=>    Func<int, int>
+    delegate bool compareDel(int x, int y); //=>    Func<int,int,bool>
+    delegate void sansParamDel();           //=>    Action
+    delegate void complexDel(int[] array);  //=>    Action<int[]>
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> delegate_noLambda = delegate (int x) { return x * x; };
+            Func<int, int> delegate_lambda = x => x * x;
+
+            Func<int, int, bool> plusGrandQue_noLambda = delegate (int left, int rigth) { return left > rigth; };
+            Func<int, int, bool> plusGrandQue_lambda = (left, rigth) => left > rigth;
+
+            Action direBonjour_noLambda = delegate () { Console.WriteLine("Bonjour"); };
+            Action direBonjour_lambda = () => Console.WriteLine("Bonjour");
+
+            Action<int[]> afficheTableau_noLambda = delegate (int[] ints)
+            {
+                foreach (int i in ints)
+                {
+                    Console.WriteLine(i);
+                }
+            };
+            Action<int[]> afficheTableau_lambda = (ints) => {
+                foreach (int i in ints)
+                {
+                    Console.WriteLine(i);
+                }
+            };
+
+            Func<int[], Func<int, bool>, int[]> filtre_numero = (ints, func) =>
+            {
+                List<int> result = new List<int>();
+                foreach (int i in ints)
+                {
+                    if (func(i)) result.Add(i);
+                }
+                return result.ToArray();
+            };
+
+            int[] test_num = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            //Utilisation de lambda dans les paramètres pertmettent un changement de comportement radical de notre fonction "filtre_numero"
+
+            int[] test_num1 = filtre_numero(test_num, (nb) => (nb % 2) != 0);
+            int[] test_num2 = filtre_numero(test_num, (nb) => nb > 2);
+            int[] test_num3 = filtre_numero(test_num, (nb) => nb == 5);
+            int[] test_num4 = filtre_numero(test_num, (nb) => (nb % 2) == 0);
+        }
+    }
+}
---
+++ Demo-LinQ-TypeAnonyme/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Demo_LinQ_TypeAnonyme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne moi = new Personne() {
+                RegNat = "123456-123-12",
+                LastName = "Legrain",
+                FirstName = "Samuel",
+                Email = "samuel.legrain@bstorm.be",
+                Genre = "Male",
+                Telephone = "080033800"
+            };
+
+            Console.WriteLine($"Bonjour {moi.LastName}, vous êtes un ${moi.Genre}.");
+
+            var moi_simple = new { 
+                moi.LastName,
+                moi.FirstName
+                //Attention: Pas possible de récupérer une méthode dans un type anonyme
+                //moi.DireBonjour
+            };
+
+            var moi_administratif = new
+            {
+                RegistreNationnal = moi.RegNat,
+                moi.Genre
+            };
+
+            Console.WriteLine($"Bonjour {moi_simple.LastName}, vous êtes un ${moi_administratif.Genre}.");
+
+            //Attention: Une propriété d'un type anonyme est en lecture seule
+            //moi_simple.LastName = "Toto";
+
+        }
+    }
+}
